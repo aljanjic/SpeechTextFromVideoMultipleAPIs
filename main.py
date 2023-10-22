b4024227ffe83fe524a1973b6cbd46e549c1f2ba
aljanjic <96263471+aljanjic@users.noreply.github.com>
2023-10-22 16:50:25 +0000

    Adding more testing materials

--- main.py
+++ main.py
@@ -29,15 +29,4 @@ except sr.RequestError as e:
       "Could not request results from Google Speech Recognition service; {0}".
       format(e))
 
-# # recognize speech using IBM Speech to Text
-# IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-# IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-# try:
-#   print("IBM Speech to Text thinks you said " +
-#         r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-# except sr.UnknownValueError:
-#   print("IBM Speech to Text could not understand audio")
-# except sr.RequestError as e:
-#   print(
-#       "Could not request results from IBM Speech to Text service; {0}".format(
-#           e))
+
